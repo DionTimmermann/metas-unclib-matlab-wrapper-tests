fb8960c81e1f0eca1268892fee6cade032b28f00
timmer01 <dion.timmermann@ptb.de>
2021-06-08 14:52:45 +0200

    Changed the order of tests to make it easier to see the results of scalar/vector multiplication.

--- unit_tests_multiplication.m
+++ unit_tests_multiplication.m
@@ -17,12 +17,14 @@
 global unc;
 unc = @LinProp;
 %% 1. .* multiplication (times)
+% 1.1. Multiplication of a Scalar and a Vector
 
 % Vector/Scalar Multiplication
 compare_a_dbl_unc(rand(1), rand(1, 3), 'a=a.*b;');
 compare_a_dbl_unc(rand(1, 3), rand(1), 'a=a.*b;');
 compare_a_dbl_unc(rand(1), rand(3, 1), 'a=a.*b;');
 compare_a_dbl_unc(rand(3, 1), rand(1), 'a=a.*b;');
+% 1.2. Multiplication of a Vector and a Vector
 
 % Vector/Vector Multiplication (Correct number of elements)
 compare_a_dbl_unc(rand(1, 3), rand(1, 3), 'a=a.*b;');
@@ -35,13 +37,27 @@ compare_a_dbl_unc(rand(1, 4), rand(1, 3), 'a=a.*b;');
 compare_a_dbl_unc(rand(1, 4), rand(3, 1), 'a=a.*b;');
 compare_a_dbl_unc(rand(4, 1), rand(1, 3), 'a=a.*b;');
 compare_a_dbl_unc(rand(4, 1), rand(3, 1), 'a=a.*b;');
-
 %% 2. * multiplication (mtimes)
-% Scalar/Vector Multiplication
+% 2.1. Multiplication of a Scalar and a Vector
+
+% Real Scalar and Vector
 compare_a_dbl_unc(3, rand(1, 3), 'a=a*b;');
 compare_a_dbl_unc(rand(1, 3), 3, 'a=a*b;');
 compare_a_dbl_unc(3, rand(3, 1), 'a=a*b;');
 compare_a_dbl_unc(rand(3, 1), 3, 'a=a*b;');
+
+% Complex Scalar/Vector
+compare_a_dbl_unc(3+4j, rand(1, 3), 'a=a*b;');
+compare_a_dbl_unc(rand(1, 3)+1j*rand(1, 3), 3, 'a=a*b;');
+compare_a_dbl_unc(3+4j, rand(3, 1), 'a=a*b;');
+compare_a_dbl_unc(rand(3, 1)+1j*rand(3, 1), 3, 'a=a*b;');
+
+% Scalar/Complex Vector
+compare_a_dbl_unc(3, rand(1, 3)+1j*rand(1, 3), 'a=a*b;');
+compare_a_dbl_unc(rand(1, 3), 3+4j, 'a=a*b;');
+compare_a_dbl_unc(3, rand(3, 1)+1j*rand(3, 1), 'a=a*b;');
+compare_a_dbl_unc(rand(3, 1), 3+4j, 'a=a*b;');
+% 2.2. Multiplication of a Vector and a Vector
 
 % Vector/Vector Multiplication
 compare_a_dbl_unc(rand(1, 3), rand(1, 3), 'a=a*b;');
@@ -49,29 +65,19 @@ compare_a_dbl_unc(rand(3, 1), rand(1, 3), 'a=a*b;');
 compare_a_dbl_unc(rand(1, 3), rand(3, 1), 'a=a*b;');
 compare_a_dbl_unc(rand(3, 1), rand(3, 1), 'a=a*b;');
 
-% Complex Scalar/Vector Multiplication
-compare_a_dbl_unc(3+4j, rand(1, 3), 'a=a*b;');
-compare_a_dbl_unc(rand(1, 3)+1j*rand(1, 3), 3, 'a=a*b;');
-compare_a_dbl_unc(3+4j, rand(3, 1), 'a=a*b;');
-compare_a_dbl_unc(rand(3, 1)+1j*rand(3, 1), 3, 'a=a*b;');
-
 % Complex Vector/Vector Multiplication
 compare_a_dbl_unc(rand(1, 3)+1j*rand(1, 3), rand(1, 3), 'a=a*b;');
 compare_a_dbl_unc(rand(3, 1)+1j*rand(3, 1), rand(1, 3), 'a=a*b;');
 compare_a_dbl_unc(rand(1, 3)+1j*rand(1, 3), rand(3, 1), 'a=a*b;');
 compare_a_dbl_unc(rand(3, 1)+1j*rand(3, 1), rand(3, 1), 'a=a*b;');
 
-% Scalar/Complex Vector Multiplication
-compare_a_dbl_unc(3, rand(1, 3)+1j*rand(1, 3), 'a=a*b;');
-compare_a_dbl_unc(rand(1, 3), 3+4j, 'a=a*b;');
-compare_a_dbl_unc(3, rand(3, 1)+1j*rand(3, 1), 'a=a*b;');
-compare_a_dbl_unc(rand(3, 1), 3+4j, 'a=a*b;');
-
 % Vector/Complex Vector Multiplication
 compare_a_dbl_unc(rand(1, 3), rand(1, 3)+1j*rand(1, 3), 'a=a*b;');
 compare_a_dbl_unc(rand(3, 1), rand(1, 3)+1j*rand(1, 3), 'a=a*b;');
 compare_a_dbl_unc(rand(1, 3), rand(3, 1)+1j*rand(3, 1), 'a=a*b;');
 compare_a_dbl_unc(rand(3, 1), rand(3, 1)+1j*rand(3, 1), 'a=a*b;');
+
+% 2.2. Multiplication of a Matricies with more than 2 dimensions
 
 % Three-dimensional mutiplication
 compare_a_dbl_unc(rand(3, 3, 3), rand(3, 3, 3), 'a=a*b;');
