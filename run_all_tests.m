10bc1c141c53e3262dd55abdb3982b5edb9ae3cb
Dion Timmermann <dion.timmermann@ptb.de>
2021-08-06 16:23:14 +0200

    Added summary to report and improved behavior in 2019b and prior.

--- run_all_tests.m
+++ run_all_tests.m
@@ -44,13 +44,51 @@ end
 cd('..');
 
 v=ver('matlab');
-filename = sprintf('%s-test-report-MATLAB-%s.xlsx', datestr(now,'yyyy-mm-dd'), v.Release(2:end-1));
-%%
-delete(filename);
-warning('off','MATLAB:xlswrite:AddSheet'); %optional
-writetable(testReportFailed,filename,'Sheet','Failed Tests');
-writetable(testReportWarning,filename,'Sheet','Warnings');
-writetable(testReportAccepted,filename,'Sheet','Accepted Differences');
-writetable(testReportPassed,filename,'Sheet','Passed Tests');
+filename = sprintf('reports/%s-test-report-MATLAB-%s.xlsx', datestr(now,'yyyy-mm-dd'), v.Release(2:end-1));
 
-winopen(filename);
+%%
+
+if verLessThan('matlab', '9.8')
+    summary = readtable('report_template.xlsx', 'ReadVariableNames',false);
+else
+    summary = readtable('report_template.xlsx', 'ReadVariableNames',false,'Format','auto');
+end
+summary.Var2 = string(summary.Var2);
+summary{1, 2} = string(v.Release(2:end-1));
+summary{2, 2} = string(datestr(now,'yyyy-mm-dd'));
+summary{3, 2} = string(datestr(now,'hh:MM'));
+
+summary{5, 2} = height(testReportFailed);
+summary{6, 2} = height(testReportWarning);
+summary{7, 2} = height(testReportAccepted);
+summary{8, 2} = height(testReportPassed);
+%%
+tryWrite = true;
+writeSuccess = false;
+while tryWrite
+    try
+        copyfile('report_template.xlsx', filename);
+        warning('off','MATLAB:xlswrite:AddSheet'); %optional
+        writetable(summary,filename,'Sheet','Summary', 'WriteVariableNames', false);
+        writetable(testReportFailed,filename,'Sheet','Failed Tests');
+        writetable(testReportWarning,filename,'Sheet','Warnings');
+        writetable(testReportAccepted,filename,'Sheet','Accepted Differences');
+        writetable(testReportPassed,filename,'Sheet','Passed Tests');
+        tryWrite = false;
+        writeSuccess = true;
+    catch
+        opts.Interpreter = 'none';
+        opts.Default = 'Retry';
+        quest = 'Could not write error report.';
+        answer = questdlg(quest,'File access error',...
+                          'Retry','Cancel',opts);
+        if ~strcmp(answer, 'Retry')
+            tryWrite = false;
+        end
+    end
+end
+
+if writeSuccess
+    winopen(filename);
+end
+    
